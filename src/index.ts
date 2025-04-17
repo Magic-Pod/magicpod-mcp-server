f9ed1c3a38fcc6f1de6f97d822e8c651f5525ccb
d6ms <g.denden.ms@gmail.com>
2025-04-17 18:34:07 +0900

    prepare for publishing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { Command } from "commander";
 import { searchMagicpodArticles } from "./tools/search-magicpod-articles.js";
