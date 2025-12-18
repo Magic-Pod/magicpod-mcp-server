e6fa5e4b056e795a55975bf95737fd037a00304c
d6ms <g.denden.ms@gmail.com>
2025-12-18 10:24:13 +0900

    build bump version

--- src/openapi-mcp-server/mcp/proxy.ts
+++ src/openapi-mcp-server/mcp/proxy.ts
@@ -55,7 +55,7 @@ export class MCPProxy {
     private otherTools: OtherToolDefinition<any>[],
   ) {
     this.server = new Server(
-      { name, version: "0.1.11" },
+      { name, version: "0.1.12-proxy-1217.0" },
       { capabilities: { tools: {} } },
     );
     const baseUrl = openApiSpec.servers?.[0].url;
