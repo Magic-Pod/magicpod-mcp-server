d424606eb94e5dd6e5f85d14592c85abed0d3c15
Sarah Just <sarahjust333@gmail.com>
2026-01-07 17:44:25 +0900

    Adds list settings tool

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import { apiV1_0UploadFileCreate } from "./tools/api-v1-0-upload-file-create.js"
 import { apiV1_0UploadDataPatterns } from "./tools/api-v1-0-upload-data-patterns.js";
 import { featureFlags } from "./config/feature-flags.js";
 import { apiV1_0CreateAutopilotTasks } from "./tools/api-v1-0-create-autopilot-tasks.js";
+import { apiV1_0ListTestSettings } from "./tools/api-v1-0-list-test-settings.js";
 
 const program = new Command();
 program.option("--api-token <key>", "MagicPod API token to use");
@@ -40,6 +41,7 @@ async function main() {
   const proxy = await initMagicPodApiProxy(baseUrl, options.apiToken, [
     apiV1_0UploadFileCreate(baseUrl, options.apiToken),
     apiV1_0UploadDataPatterns(baseUrl, options.apiToken),
+    apiV1_0ListTestSettings(baseUrl, options.apiToken),
     searchMagicpodArticles(),
     readMagicpodArticle(),
     ...(featureFlags.enableAutopilotTasks
